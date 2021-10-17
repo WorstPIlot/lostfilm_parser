6c58d8eb60c1fab391019ca1ea8dd2d1421bd4fc
WorstPilot <ninetailedfirefox@gmail.com>
2021-10-17 18:50:47 +0300

    added script

---
+++ lostfilm_parcer_bot.py
@@ -0,0 +1,226 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import telebot
+import os
+from datetime import datetime
+from telebot import types
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.relative_locator import locate_with
+from credentials import mail, password
+
+bot = telebot.TeleBot('941217243:AAEpz16h-utmjtWiZW1sUTWS8lGeTC2NYx4')
+temp_dict = {}
+
+
+def exists(filename):
+    # Проверяем существет ли файл
+    if os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)):
+        return True
+    else:
+        return False
+
+
+def write_file_from_dict(dict_name, filename):
+    with open(filename, 'w', encoding='utf-8') as file:
+        for item in dict_name.items():
+            file.write(str(item) + '\n')
+
+
+def read_saved_dict(filename):
+    if exists(filename):
+        with open(filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                symbol = ['(', ')', '\'', '\n']
+                for i in symbol:
+                    line = line.replace(i, '')
+                userid = line.split(',')[0]
+                firstname = line.split(',')[1]
+                temp_dict[userid] = firstname
+        return True
+    else:
+        return False
+
+
+def save_dict_to_file(dict_name, filename):
+    read_saved_dict(filename)
+    if temp_dict != dict_name:
+        write_file_from_dict(dict_name, filename)
+    temp_dict.clear()
+
+
+def you_are_not_admin(chat_id):
+    text = '❌ В доступе отказано id ' + str(chat_id) + ' нет в списке администраторов!'
+    return text
+
+
+def del_user_from_dict(user_key):
+    del users[str(user_key)]
+    save_dict_to_file(users, 'users.txt')
+
+
+def time_date_now():
+    return str(datetime.date(datetime.now())) + ' ' + str(datetime.time(datetime.now()))
+
+
+read_saved_dict('users.txt')
+users = temp_dict.copy()
+print(users)
+if len(temp_dict) > 0:
+    temp_dict.clear()
+read_saved_dict('admins.txt')
+admins = temp_dict.copy()
+print(admins)
+if len(temp_dict) > 0:
+    temp_dict.clear()
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    text = ''
+    if str(message.chat.id) not in users.keys():
+        text = '''Нажимая кнопку "Согласен" вы подтверждаете, что
+        1)Обязуетесь не посылать много запросов в короткий промежуток времени
+        2)Не собираетесь использовать сервис во вред для администратора сервиса
+        3)Понимаете что в случае если вы будете уличены в нарушении первых двух пунктов, можете быть забанены, и ваши
+        запросы перестанут обрабатываться, временно или насовсем
+        4)Разрешаете сервису, записать и хранить ваш id и Имя в telegram, в течении неограниченного времени.
+        Нажав кнопку "Не Согласен" вы не получите доступа к сервису.'''
+        text = ('✅ ' + str(message.chat.id) + ' добавлен в список пользователей!')
+        users[str(message.chat.id)] = message.chat.first_name
+        save_dict_to_file(users, 'users.txt')
+    elif str(message.chat.id) in users.keys():
+        text = '✅ id ' + str(message.chat.id) + ' мы уже знакомы)'
+    bot.send_message(message.chat.id, text)
+    print(users)
+
+
+@bot.message_handler(commands=['help'])
+def start_message(message):
+    bot.send_message(message.chat.id, '''/search для поискового запроса
+    /help для вывода этой справки''')
+    
+
+@bot.message_handler(commands=['search'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Хорошо, какой сериал мы ищем?')
+    bot.register_next_step_handler(message, search_tv_shows)
+
+
+def search_tv_shows(message):
+    bot.send_message(message.chat.id, 'Выполняю запрос...')
+    chrome_options = Options()
+    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('user-data-dir=' + str(message.chat.id))
+    with webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options) as driver:
+        url = 'https://www.lostfilm.tv'
+        driver.get(url)
+        print(driver.title)
+        links_on_start_page = driver.find_elements(By.CLASS_NAME, "link")
+        if links_on_start_page[4].text == 'Вход':
+            links_on_start_page[4].click()
+            driver.find_element(By.NAME, 'mail').send_keys(mail)
+            driver.find_element(By.NAME, 'pass').send_keys(password)
+            driver.find_element(By.CLASS_NAME, 'primary-btn').click()
+        driver.get(url)
+        search_box = driver.find_element(By.NAME, 'q')
+        search_box.clear()
+        search_box.send_keys(message.text)
+        search_box.submit()
+        if driver.title != 'Результаты поиска по запросу \'' + message.text.lower() + '\'':
+            bot.send_message(message.chat.id, 'К сожалению сервис не смог обработать ваш запрос, попробуйте '
+                                              'пожалуйста позже')
+            print(driver.title)
+            print('Результаты поискаHow to hide ReplyKeyboardMarkup по запросу \'' + message.text + '\'')
+        else:
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+            result_list = []
+            for name in driver.find_elements(By.CLASS_NAME, 'name-ru'):
+                if name.text.isupper():
+                    result_list.append(name.text)
+            if len(result_list) != 0:
+                for name in result_list:
+                    markup.add(types.KeyboardButton(name))
+                bot.send_message(message.chat.id, 'Вот что предлагает lostfilm по вашему запросу:', reply_markup=markup)
+            else:
+                bot.send_message(message.chat.id, 'К сожалению по вашему запросу мы не нашли не одного сериала, '
+                                                  'но вы можете попробовать уточнить запрос, воспользовавшись '
+                                                  'командой /search')
+            bot.register_next_step_handler(message, find_seasons)
+
+
+def find_seasons(message):
+    bot.send_message(message.chat.id, 'Выполняю запрос...')
+    chrome_options = Options()
+    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('user-data-dir=' + str(message.chat.id))
+    with webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options) as driver:
+        url = 'https://www.lostfilm.tv'
+        driver.get(url)
+        print(driver.title)
+        search_box = driver.find_element(By.NAME, 'q')
+        search_box.clear()
+        search_box.send_keys(message.text)
+        search_box.submit()
+        if driver.title != 'Результаты поиска по запросу \'' + message.text.lower() + '\'':
+            bot.send_message(message.chat.id, 'К сожалению сервис не смог обработать ваш запрос, попробуйте '
+                                              'пожалуйста позже')
+            print(driver.title)
+            print('Результаты поиска по запросу \'' + message.text + '\'')
+        else:
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+            print(driver.title)
+            driver.find_element(By.CLASS_NAME, 'name-ru').click()
+            print(driver.title)
+            global tv_show_url
+            tv_show_url = driver.current_url
+            driver.find_elements(By.CLASS_NAME, 'item')[6].click()
+            seasons_list = driver.find_elements(By.TAG_NAME, 'h2')
+            for season in seasons_list[1:]:
+                markup.add(types.KeyboardButton(season.text))
+            bot.send_message(message.chat.id, 'Какой сезон вы хотите?:', reply_markup=markup)
+            bot.register_next_step_handler(message, search_for_torrents)
+            global tv_show_name
+            tv_show_name = driver.find_element(By.CLASS_NAME, 'title-ru').text
+
+
+def search_for_torrents(message):
+    bot.send_message(message.chat.id, 'Выполняю запрос...')
+    chrome_options = Options()
+    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('user-data-dir=' + str(message.chat.id))
+    with webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options) as driver:
+        driver.get(tv_show_url)
+        print(driver.title)
+        if not tv_show_name.lower() in driver.title.lower():
+            bot.send_message(message.chat.id, 'К сожалению сервис не смог обработать ваш запрос, попробуйте '
+                                              'пожалуйста позже')
+            print(driver.title)
+            print('Результаты поиска по запросу \'' + message.text + '\'')
+        else:
+            print(driver.title)
+            driver.find_elements(By.CLASS_NAME, 'item')[6].click()
+            seasons_list = driver.find_elements(By.TAG_NAME, 'h2')
+            for season in seasons_list:
+                if season.text == message.text:
+                    break
+            download_season = driver.find_element(locate_with(By.CLASS_NAME, 'external-btn').below(season))
+            if download_season.get_attribute('class') == 'external-btn inactive':
+                driver.close()
+                bot.send_message(message.chat.id, 'Извините сезон ещё не завершён, пока этого не произойдёт, скачать '
+                                                  'сезон не получиться.')
+            else:
+                download_season.click()
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
+                text = ''
+                for i in driver.find_elements(By.TAG_NAME, 'a'):
+                    if i.text != '':
+                        text += i.text + '\n'
+                bot.send_message(message.chat.id, text)
+
+
+bot.polling()
